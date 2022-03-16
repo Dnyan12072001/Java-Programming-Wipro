d17f68f5b49d18e7c6184ab1088c1ce223d7387d
Dnyandeo Sanjay Narkhede <78201974+Dnyan12072001@users.noreply.github.com>
2022-03-16 13:24:26 +0530

    Add files via upload

---
+++ Assignment03.java
@@ -0,0 +1,14 @@
+
+public class Assignment03 {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		int c = a + b;
+		
+		System.out.println("The sum of " +a+" and " +b+ " is " +c );
+
+	}
+
+}
